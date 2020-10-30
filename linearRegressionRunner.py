e4f388d6b52e05b03727da8a25e1f9c6d42172e0
Elijah Flinders Desktop <elijah.flinders@mines.sdsmt.edu>
2020-10-29 19:49:19 -0600

    Fixed graph look

--- linearRegressionRunner.py
+++ linearRegressionRunner.py
@@ -13,6 +13,8 @@ df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/
 
 #Linear regression runner
 reg = LinearRegression()
+#Perceptron
+pn = Perceptron(0.1,10)
 
 #setting the indpended variable to price and the dependent to carat
 x = (df['price']-df['price'].mean())/df['price'].std()
@@ -20,7 +22,7 @@ y = (df['carat']-df['carat'].mean())/df['carat'].std()
 #fit the linear regression prediction line to the graph
 params = reg.fit(x,y)
 #add the data to the plot
-plt.scatter(x[:53940],y[:53940])
+plt.scatter(x[:53940],y[:53940], s = 1)
 pred = np.matmul(np.array(x[:53940]).reshape(-1,1),params[0])+params[1]
 plt.plot(x[:53940],pred, color = 'red')
 
@@ -29,3 +31,5 @@ plt.title('Linear Regression: Diamond Data Set')
 plt.xlabel('Price')
 plt.ylabel('Carat')
 plt.show()
+
+#pn.fit(x,y)
